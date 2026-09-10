338240a7e6fe917681ba88b03b6b5b87fa32c11f
MariiaRoma <106673607+MariiaRoma@users.noreply.github.com>
2026-09-10 10:31:28 -0500

    Add files via upload
    No update CACHE_NAME fixed

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
  * returning visitors pick up the new version instead of a stale cache.
  */
 
-const CACHE_NAME = "tariff-watch-v2026-09-02";
+const CACHE_NAME = "tariff-watch-v2026-09-09c";
 const APP_SHELL = [
   "./",
   "./index.html",
